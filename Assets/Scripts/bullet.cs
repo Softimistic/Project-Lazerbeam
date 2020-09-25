f37b4eb1db92981d7155f2904c7cb9d24e5bc41a
mennovanemmerik <51542533+mennovanemmerik@users.noreply.github.com>
2020-09-25 15:46:29 +0200

    bullet scripts added

---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{ 
+    private Rigidbody rb;
+    float speed = 20.0f;
+
+    // Start is called before the first frame update  
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        StartCoroutine(SelfDestruct());
+        rb = this.GetComponent<Rigidbody>();
+        rb.velocity = transform.up * speed;
+    }
+
+    IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/createBullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class createBullet : MonoBehaviour
+{
+    
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+
+            shootBullet();
+        }
+       
+    }
+    public void shootBullet()
+    {
+        //Bullet gets called with position/rotation of gun object
+         GameObject b = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+         
+    
+    }
+}
