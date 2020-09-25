cde87346bdc0549461abddfc7a08559904ba0642
Bestitz <xuruikang6@hotmail.com>
2020-09-25 15:34:07 +0200

    - add basic EnemyAI movement

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -11,9 +11,9 @@ public class EnemyAI : MonoBehaviour
 
     public GameObject plane;
     
-    public Transform aroundPoint;//围绕的物体
-    public float angularSpeed;//角速度
-    public float aroundRadius;//半径
+    public Transform aroundPoint;//The object that AI around 
+    public float angularSpeed;//sp
+    public float aroundRadius;//Radius
     [Tooltip("Angular velocity in degrees per seconds")]
     public float degPerSec = 60.0f;
 
@@ -22,29 +22,14 @@ public class EnemyAI : MonoBehaviour
 
     private float angled;
     // Start is called before the first frame update
-    void Start()
-    {
-        //設置物體初始位置為圍繞物體的正前方距離為半徑的點
-        // Vector3 p = aroundPoint.rotation * Vector3.forward * aroundRadius;
-        // transform.position = new Vector3(p.x, aroundPoint.position.y, p.z);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void LateUpdate()
     {
-        //GameObject plane = GameObject.FindGameObjectWithTag("PlayerPlane");
-        //if the player's plane pass the certain line, the enemy plane will spawn
         if (transform.position.z - plane.transform.position.z <= targetDistance)
         {
             Vector3 newPosition = transform.position;
-            angled += (angularSpeed * Time.deltaTime) % 360;//累加已經轉過的角度
-            newPosition.x = aroundRadius * Mathf.Sin(angled * Mathf.Deg2Rad);//計算x位置
-            newPosition.y = aroundRadius * Mathf.Cos(angled * Mathf.Deg2Rad);//計算y位置
+            angled += (angularSpeed * Time.deltaTime) % 360;
+            newPosition.x = aroundRadius * Mathf.Sin(angled * Mathf.Deg2Rad);//x position
+            newPosition.y = aroundRadius * Mathf.Cos(angled * Mathf.Deg2Rad);//y position
             newPosition.z = plane.transform.position.z + targetDistance;
             transform.position = newPosition ;
              transform.Rotate(rotAxis, degPerSec * Time.deltaTime);
