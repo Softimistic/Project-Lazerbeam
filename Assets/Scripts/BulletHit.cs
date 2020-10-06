57525877a3aa7b6794e3cfd4e70d76418f51752c
Bestitz <xuruikang6@hotmail.com>
2020-10-06 23:43:37 +0200

    - elaborated comments and replaced Update() by FixedUpdate()

--- Assets/Scripts/BulletHit.cs
+++ Assets/Scripts/BulletHit.cs
@@ -7,23 +7,34 @@ using UnityEngine;
 using UnityEngine.Serialization;
 
 public class BulletHit : MonoBehaviour
-{ 
+{
+    /// <summary>
+    /// the color while the object is hit by bullets
+    /// </summary>
     [SerializeField] private Color hitColor;
-    [SerializeField] private int hitTimes;
+    /// <summary>
+    /// after being hit by bullets for hitTimes, will trigger some thing
+    /// </summary>
+    [SerializeField][Range(0,100)] private int hitTimes;
+
     private MeshRenderer _meshRenderer;
     private Color _originalColor;
+
     private int _currentHitTimes = 0;
+
     // Start is called before the first frame update
     void Start()
     {
+        //init Renderer & originColor
         _meshRenderer = GetComponent<MeshRenderer>();
         _originalColor = _meshRenderer.material.color;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (_currentHitTimes >= hitTimes)
+        //trigger
+        if (_currentHitTimes >= hitTimes && hitTimes != 0)
         {
             //do sth here
             StartCoroutine(SelfDestroy());
@@ -37,23 +48,24 @@ public class BulletHit : MonoBehaviour
             _currentHitTimes++;
             //do sth here(eg: AUDIO)
             _meshRenderer.material.color = hitColor;
-            StartCoroutine(Delay(_originalColor));
+            StartCoroutine(RestoreColor(_originalColor));
             //Destory bullets
             Destroy(collision.gameObject);
         }
     }
-    
+
     IEnumerator SelfDestroy()
     {
         yield return new WaitForEndOfFrame();
-        //do sth here(eg: explode)
+        //do sth here(eg: explode effect/animation)
         Destroy(gameObject);
     }
-    
-    IEnumerator Delay(Color b) {
-        while(_meshRenderer.material.color != b)
+
+    IEnumerator RestoreColor(Color originColor)
+    {
+        while (_meshRenderer.material.color != originColor)
         {
-            _meshRenderer.material.color = Color.Lerp(_meshRenderer.material.color, b, Time.deltaTime);
+            _meshRenderer.material.color = Color.Lerp(_meshRenderer.material.color, originColor, Time.deltaTime);
             yield return 0;
         }
     }
