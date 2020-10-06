b8b049963c14810fb9035bcb25fce60f0fe1606e
Jorik Taal <18069118@student.hhs.nl>
2020-10-06 23:03:07 +0200

    Henk

--- Assets/Scripts/HenkRailMovement.cs
+++ Assets/Scripts/HenkRailMovement.cs
@@ -6,22 +6,27 @@ using UnityEngine;
 public class HenkRailMovement: MonoBehaviour
 {
     private Transform _transform; //Tranform attached to Henk
+    private Vector3 _virtualRotation;
+    private Rigidbody _henkBody;
     private bool[] _allowRotationReset; //Wether or not resetting the rotation is allowed ([0] is for X axis, [1] for Y and Z)
     public float Speed; //Speed at which Henk moves horizontally and vertically
     public float ForwardSpeed; //Speed at which Henk automatically moves forward
 
     void Start()
     {
+        _henkBody = GetComponent<Rigidbody>();
         _transform = GetComponent<Transform>();
+        _virtualRotation = new Vector3(0, 0, 0);
     }
 
     void Update()
     {
-        print("X: " + _transform.rotation.eulerAngles.x + ", Y:" + _transform.rotation.eulerAngles.y + ", Z:" + _transform.rotation.eulerAngles.z);
+        //print("X: " + _transform.rotation.eulerAngles.x + ", Y:" + _transform.rotation.eulerAngles.y + ", Z:" + _transform.rotation.eulerAngles.z);
 
         _allowRotationReset = new bool[] { true, true };
         Move();
         ResetTilt();
+        //AddVirtualRotationToTransform();
     }
 
     /// <summary>
@@ -38,23 +43,26 @@ public class HenkRailMovement: MonoBehaviour
             _allowRotationReset[0] = false;
         }
 
-        _transform.SetPositionAndRotation(new Vector3( //Move Henk, and apply rotation fitting that movement
+        /*_transform.SetPositionAndRotation(new Vector3( //Move Henk, and apply rotation fitting that movement
             _transform.position.x + Input.GetAxis("Horizontal") * Speed,
             _transform.position.y + Input.GetAxis("Vertical") * Speed,
             _transform.position.z + ForwardSpeed),
-            Quaternion.Euler(new Vector3(
-                _transform.rotation.eulerAngles.x - Input.GetAxis("Vertical") * Speed,
-                _transform.rotation.eulerAngles.y + Input.GetAxis("Horizontal") * Speed,
-                _transform.rotation.eulerAngles.z - Input.GetAxis("Horizontal") * Speed)));
+            _transform.rotation);*/
+
+        _henkBody.AddForce(new Vector3(
+            Input.GetAxis("Horizontal") * Speed,
+            Input.GetAxis("Vertical") * Speed, 
+            ForwardSpeed));
+
+        print(_henkBody.velocity.x + ", " + _henkBody.velocity.y + ", " + _henkBody.velocity.z);
 
         int rotationMin = -45;
         int rotationMax = 45;
-        Vector3 rotation = _transform.rotation.eulerAngles;
 
-        _transform.eulerAngles = new Vector3( //Limit rotation
-            LimitRotation(rotation.x, rotationMin, rotationMax),
-            LimitRotation(rotation.y, rotationMin, rotationMax),
-            LimitRotation(rotation.z, rotationMin, rotationMax));
+        _virtualRotation = new Vector3( //Change rotation but limit it to 45 degreess
+            LimitRotation(_virtualRotation.x - Input.GetAxis("Vertical") * Speed, rotationMin, rotationMax),
+            LimitRotation(_virtualRotation.y + Input.GetAxis("Horizontal") * Speed, rotationMin, rotationMax),
+            LimitRotation(_virtualRotation.z + Input.GetAxis("Horizontal") * Speed, rotationMin, rotationMax));
     }
 
     /// <summary>
@@ -62,21 +70,19 @@ public class HenkRailMovement: MonoBehaviour
     /// </summary>
     private void ResetTilt()
     {
-        Vector3 rotation = _transform.rotation.eulerAngles;
-
         if (_allowRotationReset[0]) //Reset X rotation
         {
-            _transform.eulerAngles = new Vector3(
-            CalcAxisReset(rotation.x, Speed),
-            _transform.rotation.eulerAngles.y,
-            _transform.rotation.eulerAngles.z);
+            _virtualRotation = new Vector3(
+            CalcAxisReset(_virtualRotation.x, Speed),
+            _virtualRotation.y,
+            _virtualRotation.z);
         }
         if (_allowRotationReset[1]) //Reset Y and Z rotation
         {
-            _transform.eulerAngles = new Vector3(
-            _transform.rotation.eulerAngles.x,
-            CalcAxisReset(rotation.y, Speed),
-            CalcAxisReset(rotation.z, Speed));
+            _virtualRotation = new Vector3(
+            _virtualRotation.x,
+            CalcAxisReset(_virtualRotation.y, Speed),
+            CalcAxisReset(_virtualRotation.z, Speed));
         }
     }
 
@@ -146,12 +152,20 @@ public class HenkRailMovement: MonoBehaviour
     }
 
     //Clamp player to camera
-    void ClampPlayer()
+    private void ClampPlayer()
     {
-        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        /*Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
         pos.x = Mathf.Clamp01(pos.x);
         pos.y = Mathf.Clamp01(pos.y);
         pos.z = Mathf.Clamp(pos.z, 2f, 10f);
-        transform.position = Camera.main.ViewportToWorldPoint(pos);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);*/
+    }
+
+    private void AddVirtualRotationToTransform()
+    {
+        _transform.eulerAngles = new Vector3(
+            _transform.eulerAngles.x + _virtualRotation.x,
+            _transform.eulerAngles.y + _virtualRotation.y,
+            _transform.eulerAngles.z + _virtualRotation.z);
     }
 }
