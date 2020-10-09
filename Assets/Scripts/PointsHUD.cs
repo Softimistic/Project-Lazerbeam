8fa9dc55528b984ee22d22f289cba57d859c7ae7
Jorik Taal <18069118@student.hhs.nl>
2020-10-09 12:25:34 +0200

    Score & Health text works properly now

---
+++ Assets/Scripts/PointsHUD.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsHUD : MonoBehaviour
+{
+    public HenkController Target_Player;
+    private Text[] _text;
+
+    // Start is ca lled before the first frame update
+    void Start()
+    {
+        _text = GetComponentsInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _text[0].text = Target_Player.SP.ToString();
+        _text[1].text = Target_Player.HP.ToString();
+    }
+}
