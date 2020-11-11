d944bf44012a87997f99061dd476f208ca686903
menno <s1106827@student.hsleiden.nl>
2020-11-11 02:26:29 +0100

    basic of enemy parrying object

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    [SerializeField] [Range(0f, 1f)]  float lerpTime;
+    [SerializeField] [Range(0f, 1f)] float lerpTime;
     [SerializeField] Color[] finalColors;
     public float targetDistance = 10.0f;
     public float enemySpeed = 0.0f;
@@ -25,6 +26,14 @@ public class EnemyAI : MonoBehaviour
     private int len;
 
     private MeshRenderer _meshRenderer;
+
+    private bool[] detect_array = {false, false, false, false, false};
+    //      0      
+    //1     2      3
+    //      4       
+    //DEZE MOETEN ANDERS WANT HET SCHIP BEWEEGT TOCH ZEIWAARDS HOND
+   
+
     // Start is called before the first frame update
      void Start()
      {
@@ -50,16 +59,62 @@ public class EnemyAI : MonoBehaviour
         if (transform.position.z - plane.transform.position.z <= targetDistance)
         {
             Vector3 newPosition = transform.position;
+            Vector3 checkedPosition = transform.position;
+
             angled += (angularSpeed * Time.deltaTime) % 360;
-            newPosition.x = aroundRadius * Mathf.Sin(angled * Mathf.Deg2Rad);//x position
-            newPosition.y = aroundRadius * Mathf.Cos(angled * Mathf.Deg2Rad);//y position
-            newPosition.z = plane.transform.position.z + targetDistance;
-            transform.position = newPosition ;
-             //transform.Rotate(rotAxis, degPerSec * Time.deltaTime);
+            newPosition.x = aroundRadius * Mathf.Sin(angled * Mathf.Deg2Rad);//x position, Als deze hoger word gaat hij naar Rechts, lager = naar links                      
+            newPosition.y = aroundRadius * Mathf.Cos(angled * Mathf.Deg2Rad);//y position, Als deze hoger word gaat hij naar Omhoog, lager = omlaag    
+
+
+
+
+
+            //check aids 
+
+            //aids = stilstaan
+
+            if (!detect_array[2] && (
+               (newPosition.y > transform.position.y && detect_array[0]) ||//als omhoog gaat (dus naar 0)
+               (newPosition.y < transform.position.y && detect_array[4]) ||//als omglaag gaat && Naar links(dus naar 4)
+
+               (newPosition.x > transform.position.x && detect_array[3])||//Naar rechts(dus naar 3)
+               (newPosition.x < transform.position.x && detect_array[4])//Naar links(dus naar 1)
+           ))
+            {
+                angled -= (angularSpeed * Time.deltaTime) % 360;
+            }
+
+
+            //Geen aids
+            else
+            {
+                checkedPosition = newPosition;
+                
+            }
+            checkedPosition.z = plane.transform.position.z + targetDistance;
+            transform.position = checkedPosition;
         }
         else
         {
             transform.position += transform.forward * enemySpeed * Time.deltaTime;
         }
+
+
+       
     }
+    public void setDetectArray(int detectPosition, bool isEnter)
+     {
+      
+
+     
+        detect_array[detectPosition] = isEnter;
+
+  //      UnityEngine.Debug.Log(detect_array[0] + " " + detect_array[1] + " " + detect_array[2] + " " + detect_array[3] + " " + detect_array[4]);
+
+
+        //detect_array[0]+" "+ detect_array[1] + " " + detect_array[2] + " " + 
+        //Makes sure that (late)Update doesnt get called for this period
+
+    }
+
 }
