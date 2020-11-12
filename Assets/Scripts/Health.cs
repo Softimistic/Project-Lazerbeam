4a72f7d07a5e86523ceacae8905c8398fdcf9b61
Essential08 <48437597+Essential08@users.noreply.github.com>
2020-11-12 15:40:27 +0100

    PlayerHealth
    Player has health and dies when he has 0 healthscore.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    
+
+    // Health begins at 100
+    int health = 100;
+    // healthText changes te text on screen
+    Text healthText;
+
+    /// Start is called before the first frame update
+    void Start()
+    {
+        // Calling the Text in Canvas.text
+        healthText = GetComponent<Text>();
+        // Converting the text from int to string
+        healthText.text = health.ToString();
+
+    }
+
+    /// <summary>
+    /// This is a global method to call at every class. You can call this to add the score.
+    /// </summary>
+    public void HealthHit(int healthDescrease)
+    {
+        health = health - healthDescrease;
+        healthText.text = health.ToString();
+
+    }
+
+    public void setHealthToZero(int setHealth)
+    {
+        health = setHealth;
+    }
+
+    public string getHealth()
+    {
+        return health.ToString();
+    }
+}
