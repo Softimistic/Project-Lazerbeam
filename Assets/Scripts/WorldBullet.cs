d02eed2245de8fdd824d28a998e6e6c1c9ed29f9
Jorik Taal <18069118@student.hhs.nl>
2020-11-16 16:21:54 +0100

    Added big gun

---
+++ Assets/Scripts/WorldBullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldBullet : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private Vector3 _originalPosition;
+    public int Speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _originalPosition = transform.position;
+        _rb.AddRelativeForce(new Vector3(0, Speed, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
