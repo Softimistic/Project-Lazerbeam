748216f36b156ef1be3d74bce3fe9057140d05f8
Essential08 <48437597+Essential08@users.noreply.github.com>
2020-11-18 13:10:00 +0100

    Shooting missles at the player

---
+++ Assets/Missle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missle : MonoBehaviour
+{
+
+    public Transform target;
+    Vector3 direction;
+
+    float speed = 50;
+    float rotationSpeed = 5;
+
+    private void Start()
+    {
+        target = GameObject.Find("Player Ship").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement of the missle
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        //Rotation
+        if(target != null)
+        {
+            direction = target.position - transform.position;
+            direction = direction.normalized;
+            // kijken naar de player
+            var rot = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/MissleTower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissleTower : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject missle;
+    int index = 0;
+
+    void Start()
+    {
+        Invoke("SpawnMissle", 5f);
+    }
+
+    void SpawnMissle()
+    {
+        GameObject newMissle = Instantiate(missle, spawnPoints[index].position, spawnPoints[index].rotation) as GameObject;
+
+        if(index < spawnPoints.Length)
+        {
+            index++;
+        }
+        if(index == 1)
+        {
+            index = 0;
+        }
+        Invoke("SpawnMissle", 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
