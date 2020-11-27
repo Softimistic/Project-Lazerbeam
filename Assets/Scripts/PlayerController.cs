460895d7ff548073f83e7f417b77f9552a8b5209
mustiej <mustiej_70@hotmail.com>
2020-11-27 03:11:56 +0100

    Henk boost&effects added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,12 +16,49 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float controlPitchFactor = -30f;
     [SerializeField] float positionYawFactor = 5f;
     [SerializeField] float controlRollFactor = -20f;
+   
+    [Header("Henk's abilities")]
+    [Tooltip("FOV when braking")] public float brakeFOV = 50f;
+    [Tooltip("FOV when boosting")] public float boostFOV = 90f;
 
+    [Header("Effects")]
+    public GameObject boostTrail;
+    public GameObject boostPop;
 
     float xThrow, yThrow;
     bool isControlEnabled = true;
-    
 
+    void Update()
+    {
+        // Boost
+        if (Input.GetButton("Fire3"))
+        {
+            Boost(true);
+        }
+
+        if (Input.GetButtonUp("Fire3"))
+        {
+            Boost(false);
+        }
+
+        // Brake
+        /*        if (Input.GetButtonDown("Fire1"))
+                {
+                    Brake(true);
+                }
+
+                if (Input.GetButtonUp("Fire1"))
+                {
+                    Brake(false);
+                }*/
+
+        // Barrel Roll
+        /*       if (Input.GetButtonDown("TriggerL"))
+               {
+                   int dir = Input.GetButtonDown("TriggerL") ? -1 : 1;
+                   QuickSpin(dir);
+               }*/
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -30,7 +67,6 @@ public class PlayerController : MonoBehaviour
             ProcessTranslation();
             ProcessRotation();
         }
-
     }
 
     void OnPlayerDeath() // Uit CollisionHandler. Zorgt ervoor dat de controls niet meer bewegen. 
@@ -69,4 +105,46 @@ public class PlayerController : MonoBehaviour
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
+
+    void Boost(bool boosting)
+    {
+        float cameraSpeed = this.transform.parent.GetComponent<BetterWaypointFollower>().routeSpeed;
+        if (boosting)
+        {
+            boostPop.SetActive(true);
+            boostTrail.SetActive(true);
+            //boostTrail.Play();
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, boostFOV, 1f * Time.deltaTime);
+            cameraSpeed = 75f;
+            //Debug.Log("speed" + cameraSpeed);
+            boostPop.GetComponent<TrailRenderer>().emitting = boosting;
+
+        }
+        else if(Camera.main.fieldOfView > 60f)
+        {
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 60f, 1f);
+            //Debug.Log("else" + Camera.main.fieldOfView);
+            cameraSpeed = 30f;
+            //Debug.Log("speed else" + cameraSpeed);
+            boostPop.SetActive(false);
+            boostTrail.SetActive(false);
+        }
+    }
+  
+/*
+    void Brake(bool braking)
+    {
+        //reduce speed
+        //reduce FOV
+    }*/
+
+    /*   public void QuickSpin(int dir)
+       {
+           if (!DOTween.IsTweening(playerModel))
+           {
+               playerModel.DOLocalRotate(new Vector3(playerModel.localEulerAngles.x, playerModel.localEulerAngles.y, 360 * -dir), .4f, RotateMode.LocalAxisAdd).SetEase(Ease.OutSine);
+               barrel.Play();
+           }
+       }*/
+
 }
