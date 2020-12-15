a696dbf69c6687164e9732e45da6118bb0e7db38
Jorik Taal <18069118@student.hhs.nl>
2020-12-15 21:11:29 +0100

    Completed basic functionality of briefing

--- Assets/Briefing.cs
+++ Assets/Briefing.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Briefing : MonoBehaviour
+public class Briefing : SceneTransitionEvent
 {
+    [Tooltip("Drag messager from UI here")] public Messager Messager; //Assign in editor 
+    private MessageEvent[] _messageEvents; //Assign in editor by adding children gameObjects
+    private int _counter = 0;
+    
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _messageEvents = GetComponentsInChildren<MessageEvent>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (_counter == 0)
+        {
+            _messageEvents[_counter].Play();
+            _counter++;
+        }
+        else if (!Messager.IsMessageActive())
+        {
+            if (_messageEvents.Length == _counter)
+            {
+                LoadNextScene();
+            }
+            else
+            {
+                _messageEvents[_counter].Play();
+                _counter++;
+            }
+        }
     }
 }
