ca134a0c18cb6fb7702d8a1980a7390f21f8bd1a
menno <s1106827@student.hsleiden.nl>
2020-12-29 21:43:29 +0100

    automatic firing(keep button pressed) + cooldown

--- Assets/Scripts/createBullet.cs
+++ Assets/Scripts/createBullet.cs
@@ -7,25 +7,19 @@ using Debug = UnityEngine.Debug;
 
 public class createBullet : MonoBehaviour
 {
-    
+
     public GameObject bulletPrefab;
     public GameObject ship;
     public GameObject camera;
-    public bool BossMode;
-    [SerializeField] private int speedfactor;
-
+    [SerializeField] [Range(2, 100)] private int speedfactor;
+    bool spacebarPressed = false;
+    public float cooldown_ms;
+    float timer_ms = 0;
     // Start is called before the first frame update
     void Start()
     {
         //set speed to bullet
-        if (BossMode)
-        {
-            bulletPrefab.GetComponent<bullet>().Speed = speedfactor;
-        }
-        else
-        {
-            bulletPrefab.GetComponent<bullet>().Speed = camera.GetComponent<BetterWaypointFollower>().routeSpeed * speedfactor;
-        }
+        bulletPrefab.GetComponent<bullet>().Speed = camera.GetComponent<BetterWaypointFollower>().routeSpeed * speedfactor;
     }
 
     // Update is called once per frame
@@ -33,13 +27,31 @@ public class createBullet : MonoBehaviour
     {
         if (Input.GetKeyDown("space") && ship.GetComponent<PlayerController>().isActiveAndEnabled)
         {
-            shootBullet();
+            spacebarPressed = true;
         }
+        else if (Input.GetKeyUp("space"))
+        {
+            spacebarPressed = false;
+        }
+
+        if (spacebarPressed)
+        {
+            if (timer_ms <= 0)
+            {
+                shootBullet();
+                timer_ms = cooldown_ms;
+            }
+        }
+        if (timer_ms > 0)
+        {
+            timer_ms -= Time.deltaTime * 1000;
+        }
+
     }
     public void shootBullet()
     {
         //Bullet gets called with position/rotation of gun object
-         GameObject b = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
-         Debug.Log("Bullet Speed : " + bulletPrefab.GetComponent<bullet>().Speed);
+        GameObject b = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+        Debug.Log("Bullet Speed : " + bulletPrefab.GetComponent<bullet>().Speed);
     }
 }
