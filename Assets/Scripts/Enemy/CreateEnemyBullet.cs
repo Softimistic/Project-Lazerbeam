6cdf6241d44b354e8b5dde61ab2dd6a40292b68e
Jorik Taal <18069118@student.hhs.nl>
2020-12-29 16:02:16 +0100

    Added alternate projectile option

--- Assets/Scripts/Enemy/CreateEnemyBullet.cs
+++ Assets/Scripts/Enemy/CreateEnemyBullet.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CreateEnemyBullet : MonoBehaviour
 {
     [SerializeField] private GameObject projectile;
+    [Header("Optional")]
+    [SerializeField] private GameObject projectileAlternate;
 
     private void Update()
     {
@@ -14,11 +16,33 @@ public class CreateEnemyBullet : MonoBehaviour
     public void Shoot(GameObject player)
     {
         Vector3 direction = (player.transform.position + player.transform.forward * 6f) - transform.position;
+
+
         Quaternion rotation = Quaternion.LookRotation(direction);
         transform.rotation = rotation * Quaternion.Euler(90, 0, 0);
 
         int randomNumber = GetComponentInParent<Enemy>().GetAccuracy();
-        Quaternion randomShootingField = Quaternion.Euler(Random.Range(transform.rotation.eulerAngles.x - randomNumber, transform.rotation.eulerAngles.x + randomNumber), Random.Range(transform.rotation.eulerAngles.y - randomNumber, transform.rotation.eulerAngles.y + randomNumber), Random.Range(transform.rotation.eulerAngles.z - randomNumber, transform.rotation.eulerAngles.z + randomNumber));
-        Instantiate(projectile, transform.position, randomShootingField);
+        Quaternion randomShootingField = Quaternion.Euler(Random.Range(transform.rotation.eulerAngles.x - randomNumber, transform.rotation.eulerAngles.x + randomNumber),
+            Random.Range(transform.rotation.eulerAngles.y - randomNumber, transform.rotation.eulerAngles.y + randomNumber),
+            Random.Range(transform.rotation.eulerAngles.z - randomNumber, transform.rotation.eulerAngles.z + randomNumber));
+
+        GameObject boolet;
+        if (projectileAlternate != null)
+        {
+            int cock = Random.Range(0, 10);
+            if (cock > 5)
+            {
+                boolet = projectile;
+            }
+            else
+            {
+                boolet = projectileAlternate;
+            }
+        }
+        else
+        {
+            boolet = projectile;
+        }
+        Instantiate(boolet, transform.position, randomShootingField);
     }
 }
