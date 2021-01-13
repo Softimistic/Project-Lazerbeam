7a15d31abbe8612b68ebf9aba0ca465cba7520a2
BuildTools <unconfigured@null.spigotmc.org>
2021-01-13 18:55:44 +0100

    changed itemdrop rng to boolean

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -133,25 +133,21 @@ public abstract class Enemy : MonoBehaviour
     {
         if (rngDrops)
         {
-            int willDrop = Random.Range(1, 101);
-            if (willDrop <= itemDropChance)
+            int itemToDrop = Random.Range(0, 3);
+            PowerUp pickup = null;
+            switch (itemToDrop)
             {
-                int itemToDrop = Random.Range(0, 3);
-                PowerUp pickup = null;
-                switch (itemToDrop)
-                {
-                    case 0:
-                        pickup = healthPowerUp;
-                        break;
-                    case 1:
-                        pickup = boostPowerUp;
-                        break;
-                    case 2:
-                        pickup = shieldPowerUp;
-                        break;
-                }
-                Instantiate(pickup, transform.position, transform.rotation);
+                case 0:
+                    pickup = healthPowerUp;
+                    break;
+                case 1:
+                    pickup = boostPowerUp;
+                    break;
+                case 2:
+                    pickup = shieldPowerUp;
+                    break;
             }
+            Instantiate(pickup, transform.position, transform.rotation);
         }
     }
 }
