8b720cb74a603baa47e9b8efdae04f28220d758a
MitchRoodakker <mroodakker@outlook.com>
2021-01-21 11:39:10 +0100

    EndingScenes Added

---
+++ Assets/Scripts/EndingTextScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingTextScript : MonoBehaviour
+{
+    AudioSource myAudio;
+    public float time = 9f;
+    bool audioplaying = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+        myAudio.time = 1.8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else if(time <= 0 && audioplaying == false)
+        {
+            Debug.Log("Play Audio Here -- Timer Over!!");
+            myAudio.Play();
+            audioplaying = true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/HankExplosionScene.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HankExplosionScene : SceneTransitionEvent
+{
+    [Tooltip("FX Prefab")] [SerializeField] ParticleSystem deathFX;
+    bool AlOntploft = false;
+    private GameObject controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("HankDies", 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void HankDies()
+    {
+        deathFX.Play();
+    }
+}
